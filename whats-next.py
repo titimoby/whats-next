41a3135ea9e65187adba3ae18e90a489fd1927ec
Thierry Chantier <titimoby@gmail.com>
2024-01-01 18:04:24 +0100

    filter todo elements on project id

--- whats-next.py
+++ whats-next.py
@@ -12,11 +12,8 @@ def get_gitlab_todos():
     # Initialize GitLab
     gl = gitlab.Gitlab(url=config.gitlab_server, private_token=config.gitlab_token)
 
-    # Get project
-    project = gl.projects.get(config.gitlab_project_id)
-
     # Get todos
-    todos = gl.todos.list(all=True)
+    todos = gl.todos.list(project_id=config.gitlab_project_id, all=True)
     return [todo.attributes for todo in todos]
 
 
